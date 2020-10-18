40c5c8dc936984f84d9308f65eb27c9d840c6715
abishek1233 <43175038+abishek1233@users.noreply.github.com>
2020-10-19 00:03:11 +0530

    hello

--- curr.py
+++ curr.py
@@ -1,6 +1,5 @@
-print('Currency to be changed' + '\r\n' + '0:USD'+ '\r\n' + '1:EURO' + '\r\n' + '2:YEN' + '\r\n' + '3:YUAN' + '\r\n' + '4:AED' + '\r\n' + '5:SGD' + '\r\n' + '6:GBP' )
-
-inr = float(input('What is the amount of INR you wish to convert?'))
+inr = float(input('What is the amount of INR you wish to convert?\n\r'))
+curr_ = int(input('choose which currency'+ '\r\n' + '0:USD'+ '\r\n' + '1:EURO' + '\r\n' + '2:YEN' + '\r\n' + '3:YUAN' + '\r\n' + '4:AED' + '\r\n' + '5:SGD' + '\r\n' + '6:GBP\n\r'))
 
 def USD():
     return inr * 0.014
@@ -23,7 +22,7 @@ def SGD():
 def GBP():
     return inr * 0.011
 
-def convert_to(inr):
+def convert_to(curr_):
     switcher={
         0:USD,
         1:EURO,
@@ -33,6 +32,7 @@ def convert_to(inr):
         5:SGD,
         6:GBP
         }
-    func = switcher.get(inr)
-    print('The amount you converted is',func())
-convert_to(4)
+
+    func = switcher.get(curr_,lambda:"Invalid Entry")
+    print('The converted amount is',func(),format(switcher))
+convert_to(curr_)
