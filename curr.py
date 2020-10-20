47a717756f66882754e63426aec459f4a8cab0c0
Aneruth Mohanasundaram <ane1998@gmail.com>
2020-10-20 11:06:14 +0530

    new changes

--- curr.py
+++ curr.py
@@ -4,27 +4,27 @@ print('Please choose from this option \n\r' + '\n\r' + '0:USD' + '\n\r' + '1:EUR
 type_curr = int(input('Choose the currency mode:') + '\n')
 
 def USD():
-    return inr * 0.014
+    return str(inr * 0.014) + ' USD'
 
 def EURO():
-    return inr * 0.012
+    return str(inr * 0.012) + ' EURO'
 
 def YEN():
-    return inr * 1.44
+    return str(inr * 1.44) + ' YEN'
 
 def YUAN():
-    return inr * 0.091
+    return str(inr * 0.091) + ' YUAN'
 
 def AED():
-    return inr * 0.050
+    return str(inr * 0.050) + ' AED'
 
 def SGD():
-    return inr * 0.019
+    return str(inr * 0.019) + ' SGD'
 
 def GBP():
-    return inr * 0.011
+    return str(inr * 0.011) + ' GBP'
 
-def convert_to(inr):
+def convert_to(type_curr):
     switcher={
         0:USD,
         1:EURO,
@@ -34,12 +34,15 @@ def convert_to(inr):
         5:SGD,
         6:GBP
         }
-    for i in switcher.items():
-        if i == type_curr:
-            i = switcher.get(type_curr)
-            
-    func = switcher.get(inr)
+    # for i in switcher.items():
+    #     if i == type_curr:
+    #         i = switcher.get(type_curr)
+    
+    func = switcher.get(type_curr)
+    print('\n')
+    print('Amount you entered',str(inr) + ' INR')
     print('\n')
     print('You chose option {0}'.format(type_curr))
+    print('\n')
     print('The amount you converted is',func())
 convert_to(type_curr)
